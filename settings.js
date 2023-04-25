8c54ee81c0d344d2f8ffc72c8cd719797e3904c2
Aldiergokil <33947049+Aldiergokil@users.noreply.github.com>
2023-04-25 11:16:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'aldiergokil'// yourname
+global.MONGO_DB_URI = "mongodb+srv://aldiergokil:oe5a8Xs5t474oZUq@cluster0.jvzhalb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "aldiergokil@gmail.com" //email
+global.email_password = "pfglropsjjddcdrc" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
